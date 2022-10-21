3c59976cce60dc0c665f36a3d8056df31ea9ac70
Oleg-GH <fan2004g@bk.ru>
2022-10-21 22:22:58 +0500

    Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа. 456 -> 5

---
+++ Task10/Program.cs
@@ -0,0 +1,15 @@
+﻿// Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает 
+// вторую цифру этого числа.  456 -> 5
+
+Console.Clear();
+Console.Write("ВВедите число от 100 до 999:  ");
+int number = int.Parse(Console.ReadLine() ?? "0");
+
+if (number > 99 && number < 1000)
+{
+    int second = number / 10 % 10;
+    Console.WriteLine($"{number} -> {second}");
+}
+else
+    Console.WriteLine("Введено некорректное значение");
+
